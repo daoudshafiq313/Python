4add019f52409653c0b48ec96d0d99ce70c4057f
daoudshafiq313 <daoudshafiq03@gmail.com>
2023-06-14 21:08:42 +0200

    first commit

---
+++ p1.py
@@ -0,0 +1,25 @@
+a = 5
+b = 10
+my_variable = 15
+any_variable_name = 100
+
+string_variable = "hello"
+single_quotes = "strings can have single quotes too"
+
+print (string_variable)
+print (my_variable)
+
+def add(a,b):
+    return a+b
+# calling function by it's name
+sum = add(2,5)
+print(sum)
+
+
+
+def add(a,b):
+    return a+b
+
+# calling function by it's name
+sum = add(3,2)
+print(sum)
